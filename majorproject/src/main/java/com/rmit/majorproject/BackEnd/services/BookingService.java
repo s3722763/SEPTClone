4de533fc312b12e3381a01fabd3c386b3a0d10be
s3722763 <s3722763@student.rmit.edu.au>
2020-08-24 20:38:09 +1000

    Implemented initial code for BookingService

---
+++ majorproject/src/main/java/com/rmit/majorproject/BackEnd/services/BookingService.java
@@ -0,0 +1,18 @@
+package com.rmit.majorproject.BackEnd.services;
+
+import com.rmit.majorproject.BackEnd.Repositories.BookingRepository;
+import com.rmit.majorproject.BackEnd.model.Booking;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingService {
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    public List<Booking> findAll() {
+        return bookingRepository.findAll();
+    }
+}
