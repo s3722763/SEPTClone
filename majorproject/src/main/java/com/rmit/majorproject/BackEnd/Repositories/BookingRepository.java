77f7957babb869f47ac6c592530787b17e83dba7
s3722763 <s3722763@student.rmit.edu.au>
2020-08-24 20:55:32 +1000

    Implemented initial code for BookingService

--- majorproject/src/main/java/com/rmit/majorproject/BackEnd/Repositories/BookingRepository.java
+++ majorproject/src/main/java/com/rmit/majorproject/BackEnd/Repositories/BookingRepository.java
@@ -1,2 +1,9 @@
-package com.rmit.majorproject.BackEnd.Repositories;public class BookingRepository {
+package com.rmit.majorproject.BackEnd.Repositories;
+
+import com.rmit.majorproject.BackEnd.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
--- majorproject/src/main/java/com/rmit/majorproject/BackEnd/model/Booking.java
+++ majorproject/src/main/java/com/rmit/majorproject/BackEnd/model/Booking.java
@@ -1,2 +1,41 @@
-package com.rmit.majorproject.BackEnd.model;public class Booking {
+package com.rmit.majorproject.BackEnd.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+import javax.persistence.GeneratedValue;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue
+    private long id;
+    private LocalDateTime bookingDate;
+
+    public Booking() {};
+
+    public Booking(LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getBookingDate() {
+        return this.bookingDate;
+    }
+
+    @Column(name = "booking_date", nullable = false)
+    public void setBookingDate(LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
+    }
 }
