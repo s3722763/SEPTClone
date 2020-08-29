cce81c584b8086abdcf63bf6e9d94c6759737131
s3722763 <s3722763@student.rmit.edu.au>
2020-08-29 15:02:11 +1000

    Improved REST responces for errors in creating bookings

--- majorproject/src/main/java/com/rmit/majorproject/BackEnd/web/BookingController.java
+++ majorproject/src/main/java/com/rmit/majorproject/BackEnd/web/BookingController.java
@@ -8,9 +8,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+import java.lang.reflect.Field;
 import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
@@ -24,7 +28,11 @@ public class BookingController {
     private BookingService bookingService;
 
     @PostMapping("")
-    public ResponseEntity<Booking> createNewBooking(@RequestBody Booking booking) {
+    public ResponseEntity<?> createNewBooking(@Valid @RequestBody Booking booking, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+        }
+
         booking.setBookingCreationDate(Date.from(Instant.now()));
         bookingService.add(booking);
 
