2f2556a0e24d3c6d3c3732cefc486a780b711565
s3722763 <s3722763@student.rmit.edu.au>
2020-09-22 10:56:35 +1000

    Updated employee model and assossiated tests to reflect front end

--- majorproject/src/main/java/com/rmit/majorproject/BackEnd/model/Employee.java
+++ majorproject/src/main/java/com/rmit/majorproject/BackEnd/model/Employee.java
@@ -1,10 +1,12 @@
 package com.rmit.majorproject.BackEnd.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rmit.majorproject.BackEnd.validators.DateConstraint;
+import org.springframework.core.annotation.AliasFor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -21,6 +23,21 @@ public class Employee {
     @ElementCollection(targetClass = String.class)
     @DateConstraint
     private List<String> potentialRosterDays;
+    @NotNull(message = "Date of birth cannot be blank")
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    private Date dateOfBirth;
+    @NotBlank(message = "Gender must be specified")
+    private String gender;
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid Email")
+    private String email;
+    @NotBlank(message = "Must include a phone number")
+    @Pattern(regexp = "(04)?[0-9]{8}")
+    private String phoneNumber;
+    @NotBlank(message = "Must include a TFN number")
+    private String TFN;
+    @NotBlank(message = "Must include super number")
+    private String superNumber;
 
     public Employee() {}
 
@@ -54,5 +71,53 @@ public class Employee {
 
     public void setPotentialRosterDays(List<String> potentialRosterDays) {
         this.potentialRosterDays = potentialRosterDays;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getTFN() {
+        return TFN;
+    }
+
+    public void setTFN(String TFN) {
+        this.TFN = TFN;
+    }
+
+    public String getSuperNumber() {
+        return superNumber;
+    }
+
+    public void setSuperNumber(String superNumber) {
+        this.superNumber = superNumber;
     }
 }
